01f84f5e7f066c9e16ebf4b2c29d8a6e5a6b6a58
jeffrey <f70129@gmail.com>
2026-07-03 10:00:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taiwan-trading-v24';
+const CACHE = 'taiwan-trading-v25';
 const ASSETS = ['./manifest.json', './manifest-mobile.json', './icon.svg'];
 
 self.addEventListener('install', (e) => {
